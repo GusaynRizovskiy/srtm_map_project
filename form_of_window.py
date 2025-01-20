94f926b5db9695413ba696b5720a9c1395bea603
Maks <makkks200426013@mail.ru>
2025-01-20 18:16:18 +0300

    Внес изменения следующего характера: Dilog заменил на first_part и название класса на Form1

--- form_of_window.py
+++ form_of_window.py
@@ -11,7 +11,7 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-class Ui_Dialog(object):
+class Form1(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
         Dialog.resize(1193, 657)
@@ -264,8 +264,8 @@ class Ui_Dialog(object):
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
-    Dialog = QtWidgets.QDialog()
-    ui = Ui_Dialog()
-    ui.setupUi(Dialog)
-    Dialog.show()
+    first_part = QtWidgets.QDialog()
+    ui = Form1()
+    ui.setupUi(first_part)
+    first_part.show()
     sys.exit(app.exec_())
